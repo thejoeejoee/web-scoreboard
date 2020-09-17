94c4c12ede6871a72325604c5a319ec4b7171b30
Josef Kolář <thejoeejoee@gmail.com>
2020-09-18 00:21:57 +0200

    Allow all in CORS

--- api/data-proxy.ts
+++ api/data-proxy.ts
@@ -3,6 +3,6 @@ import {NowRequest, NowResponse} from '@vercel/node'
 
 export default (request: NowRequest, response: NowResponse) => {
     const {name = 'World'} = request.query
-    response.setHeader('Access-Control-Allow-Origin', response.headers.origin);
+    response.setHeader('Access-Control-Allow-Origin', '*');
     response.status(200).send(JSON.stringify({data: `Hello ${name}!`}))
 }
