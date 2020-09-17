a0ac702bd420a5f9e448f1d1e98b8de8caf06a04
Josef Kolář <thejoeejoee@gmail.com>
2020-09-18 00:19:41 +0200

    Added CORS headers to API

--- api/data-proxy.ts
+++ api/data-proxy.ts
@@ -3,5 +3,6 @@ import {NowRequest, NowResponse} from '@vercel/node'
 
 export default (request: NowRequest, response: NowResponse) => {
     const {name = 'World'} = request.query
+    response.setHeader('Access-Control-Allow-Origin', response.headers.origin);
     response.status(200).send(JSON.stringify({data: `Hello ${name}!`}))
 }
