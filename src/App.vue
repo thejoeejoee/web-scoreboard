a3b8115203909eb245569d6f505f2a3430e551b9
Josef Kolář <thejoeejoee@gmail.com>
2020-09-19 12:17:17 +0200

    Add Github corner

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <GreenScreen/>
     <Controls></Controls>
+
+    <Github></Github>
   </div>
 </template>
 
 <script>
 import GreenScreen from "@/components/Greenscreen";
 import Controls from "@/components/Controls";
+import Github from "@/components/Github";
 
 export default {
   name: 'app',
   components: {
+    Github,
     Controls,
     GreenScreen,
   }
